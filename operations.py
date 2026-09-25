df8a766ef6415438e0578fad290d1181ac872d09
Albert Dark <albertkhankiev@gmail.com>
2026-09-25 12:47:55 +0300

    Initiate operations.py

---
+++ operations.py
@@ -0,0 +1,14 @@
+import math
+
+#надо реализовать
+def add(a, b):print()
+def subtract(a, b):print()
+def multiply(a, b):print()
+def divide(a, b):print()
+def modulo(a, b):print()
+def sin_deg(x):print()
+def cos_deg(x):print()
+def power(a, b):print()
+def square_root(x):print()
+def floor_value(x):print()
+def ceil_value(x):print()
