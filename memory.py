1337a19454ea91d9b898fda1494c1a2c4ebaaf25
Albert Dark <albertkhankiev@gmail.com>
2026-09-25 13:03:44 +0300

    Initiate memory.py

--- memory.py
+++ memory.py
@@ -1,8 +1,7 @@
 #функции с памятью
 #надо реализовать
 class Memory:
-    def __init__(self):
-        self.value = 0.0
+    def __init__(self): self.value = 0.0
 
     def madd(self, number):return
     def msubtract(self, number):return
